49b51eaf183a3c6b8e100fa8bfbe8c1fa6dce3cc
Ronny Ewanek <ronny.ewanek@gmail.com>
2017-09-13 11:39:26 -0500

    add survey model

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
+// set of credentials to return
 if (process.env.NODE_ENV === 'production') {
+  // if in production then return prod keys
   module.exports = require('./prod');
 } else {
   module.exports = require('./dev');
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 
 require('./models/User');
-
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
