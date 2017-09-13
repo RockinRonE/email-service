0f09175e9ee009a8f2b12aa5350559af7e400d79
Ronny Ewanek <ronny.ewanek@gmail.com>
2017-09-13 11:00:36 -0500

    add heroku config server

--- index.js
+++ index.js
@@ -27,5 +27,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // if route request is not recognized, use build
+  app.use(express.static('client/build'));
+
+  // and if not in build, then just serve index
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
